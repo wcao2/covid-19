0458bd07563cc7049956ecfa88b9d5f4e23a9641
wcao2 <caowei20171218@gmail.com>
2020-07-13 22:44:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Cards from './components/Cards/Cards';
+// import Cards from './components/Chart/Chart';
+// import Cards from './components/Country/Country';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards,Chart,CountryPicker} from './components';
+import styles from './App.module.css'; 
+
+import whoLogo from './images/whoLogo.jpg';
+
+import {fetchData} from './api';
+
+class App extends React.Component{
+    state={
+        data:{},
+        country:''
+    }
+
+    async componentDidMount(){
+        const fetchedData=await fetchData();
+        //console.log(data);
+        this.setState({data:fetchedData});
+    }
+
+    handleCountryChange=async(country)=>{
+        //fetch data
+        console.log(country);
+
+        const fetchedData=await fetchData(country);
+        //console.log(fetchedData);
+        this.setState({data:fetchedData,country:country});
+    }
+
+    render(){
+        const {data,country}=this.state;
+        //{styles.container} make sure that don't have any interference with any other CSS file
+        return(
+            <div className={styles.container}>
+                <img className={styles.image} src={whoLogo} alt="COVID-19"/>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        )
+    }
 }
 
 export default App;
